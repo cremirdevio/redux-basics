20d945a3586f60d78780d20192c8102afe496a16
cremirdevio <josephajibodu@gmail.com>
2021-12-26 00:53:01 +0100

    Bug and Projects Reducers combined
    The 2 reducers are further grouped under another reducer called entities

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ store.subscribe(() => {
   console.log("Some data are changing already");
 });
 
-// store.dispatch(bugAdded({ description: "Bug One (React Native)" }));
-// store.dispatch(bugAdded({ description: "Bug Two (React with NextJS)" }));
-// store.dispatch(bugAdded({ description: "Bug Three (React Native with expo)" }));
-// store.dispatch(bugRemoved({ id: 1 }));
-// store.dispatch(bugResolved({ id: 2 }));
+store.dispatch(bugAdded({ description: "Bug One (React Native)" }));
+store.dispatch(bugAdded({ description: "Bug Two (React with NextJS)" }));
+store.dispatch(bugAdded({ description: "Bug Three (React Native with expo)" }));
+store.dispatch(bugRemoved({ id: 1 }));
+store.dispatch(bugResolved({ id: 2 }));
 
 store.dispatch(projectAdded({
     name: "Lend Naira Web Project"
